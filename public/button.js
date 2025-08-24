e378e4d26cb1411066eeb107a701d1cc4a1d7ec3
erkin-glitch <rustamoverkin94@gmail.com>
2025-08-24 18:12:37 +0500

    add: button js to html

---
+++ public/button.js
@@ -0,0 +1,4 @@
+const element = document.getElementById("response__container")
+     function OnTap() {
+         element.style.display = (element.style.display === 'block') ? 'none' : 'block';
+        }
