497e789764493f8d23cb954151cb3b0877ec01ef
erkin-glitch <rustamoverkin94@gmail.com>
2025-09-12 21:41:28 +0500

    add: calculator to count amount

--- controllers/finance/actions/balance.js
+++ controllers/finance/actions/balance.js
@@ -1,14 +1,26 @@
-const {Transaction} = require("../../../models")
+const { Transaction } = require("../../../models");
 
 module.exports = async (req, res) => {
- try {
+  try {
     const transactions = await Transaction.findAll({
       order: [["createdAt", "DESC"]],
     });
-    res.render("balance", { transactions, user: req.session.user });
+
+    let totalBalance = 0;
+    transactions.forEach((transaction) => {
+      if (transaction.type === "income") {
+        totalBalance += parseFloat(transaction.amount);
+      } else if (transaction.type === "expense") {
+        totalBalance -= parseFloat(transaction.amount);
+      }
+    });
+
+    res.render("balance", {
+      transactions,
+      totalBalance, 
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("Ошибка при загрузке транзакций");
   }
 };
-    
